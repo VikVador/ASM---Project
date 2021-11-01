d7e3a5fe411b790a0375ff990f776379a662c78a
Romain_Gaspar <romaingaspar@gmail.com>
2021-11-01 16:46:35 +0100

    les fichiers planestress/strain1 sont ok mais les planestress/strain2 ont pas encore été faits

---
+++ ASM-Part_1/Partie 1-3/Planestrain1/material2/MAterial2.m
@@ -0,0 +1,52 @@
+% Plane strain 
+
+load EPL.ascii
+load Sigma_XX.ascii
+load SigmaVM.ascii
+load Sigma_Yield.ascii
+load time.ascii
+
+
+
+%--------------------------
+%Equivalent plastic strain
+%--------------------------
+
+figure
+plot(time,EPL)
+xlabel("time [s]")
+ylabel("EPL [-]")
+set(gca,'FontSize',20)
+
+
+
+
+%--------------------------
+% Sigma von-mises
+%--------------------------
+
+figure
+plot(time,SigmaVM)
+xlabel("time [s]")
+ylabel("\sigma_{VM} [MPa]")
+set(gca,'FontSize',20)
+
+
+
+
+
+%--------------------------
+% Yield stress
+%--------------------------
+
+figure
+plot(time,Sigma_Yield)
+xlabel("time [s]")
+ylabel("Yield stress [MPa]")
+set(gca,'FontSize',20)
+
+
+
+
+
+
